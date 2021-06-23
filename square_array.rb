94cbd77b1be65decb31048e225f8a2a20b6bb1f3
Nabil Hayet <nabil.bracu@gmail.com>
2021-06-23 14:02:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
-counter = 0
-updated = Array.new
+ counter = 0
+ updated = Array.new
 
-  array.each do |arr|
+ array.each do |arr|
   updated[counter]=array[counter]*array[counter]
   counter+=1
   puts updated
-end 
-updated
+ end 
+ updated
 end
 array = [1,2,3]
 square_array(array)
